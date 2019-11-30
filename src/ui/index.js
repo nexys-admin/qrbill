b0a78a5a2190ab0245c3c5e4e8ed437d0fc8e398
John B <john@nexys.ch>
2019-11-30 16:08:19 +0100

    added dep + license

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as UI from '@nexys/uibs4';
 
-console.log(UI)
-
 export default () => (<div>
   <p>Here you can play with different tools to understand the Swiss QR Bill</p>
   <p><b><Link to="/qr/read">Click here</Link> if you would like to read a Swiss QR Code</b></p>
