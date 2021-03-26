c88eef17076bf6fb07ac62faac049eac046b7910
johnb8005 <johnb8005@users.noreply.github.com>
2021-03-26 23:06:56 +0000

    deploy: 6aa768c20c6787b2ff91363dfc786e7103bbfd45

---
+++ precache-manifest.a3fe57e7b49ce63ce948f06de49d7fda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3329b791427086a258ad4e4cca5a0e4b",
+    "url": "/qrbill/index.html"
+  },
+  {
+    "revision": "8a7c44adecdd5f43229c",
+    "url": "/qrbill/static/js/2.4d26bb82.chunk.js"
+  },
+  {
+    "revision": "4c7d710ec08f97cd0c5a",
+    "url": "/qrbill/static/js/main.833c411a.chunk.js"
+  },
+  {
+    "revision": "43662b62102b1865c523",
+    "url": "/qrbill/static/js/runtime-main.db8cf906.js"
+  }
+]);
