97ac661d32adf1eca7f76e42e4470ce8519a872a
johnb8005 <johnb8005@users.noreply.github.com>
2022-02-26 16:15:25 +0000

    deploy: 61bdb568e15fb50646bef87bfae4cebb08c14703

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "9398cf18948c9c371505cda8acef66e4a98bcccf";
-export const SNOWPACK_PUBLIC_VERSION = "v1.2.7";
+export const SNOWPACK_PUBLIC_GIT_SHA = "61bdb568e15fb50646bef87bfae4cebb08c14703";
+export const SNOWPACK_PUBLIC_VERSION = "v1.2.8";
 export const SNOWPACK_PUBLIC_URL = "/qrbill";
 export const MODE = "production";
 export const NODE_ENV = "production";
