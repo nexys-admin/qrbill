4dce23847fd212fde1fbbaed9e0aefa801afa000
John B <johan@nexys.ch>
2022-02-26 16:20:44 +0100

    typo

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,7 +7,7 @@ import Header from './header'
 
 const Layout = ({children}:{children: JSX.Element | JSX.Element[]}) => <>
   <Header/>
-  <div className="container">{props.children}</div>
+  <div className="container">{children}</div>
   <footer><small><a href={githubUrlVersion}>{version}</a></small></footer>
 </>
  
