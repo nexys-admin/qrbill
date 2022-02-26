fe82d85cadaba1db6dc54bb483a6a2cc0b8c8be3
John B <johan@nexys.ch>
2022-02-26 16:22:11 +0100

    change import env var

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const githubUrl:string = 'https://github.com/nexys-admin/qrbill';
-export const version:string = import.meta.env.REACT_APP_VERSION || "unset_version";
+export const version:string = process.env.REACT_APP_VERSION || "unset_version";
 export const githubUrlVersion:string = `${githubUrl}/releases/tag/${version}`
 export const title = "Swiss QR Bill";
