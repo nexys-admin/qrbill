8e78c8b735414d20601dd7d107a554753ab0db92
johnb8005 <johnb8005@users.noreply.github.com>
2022-02-26 15:24:00 +0000

    deploy: 267ec4137813cd2db4081b20eece5aa137a75190

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
-
 export const githubUrl = "https://github.com/nexys-admin/qrbill";
-export const version = __SNOWPACK_ENV__.REACT_APP_VERSION || "unset_version";
+export const version = process.env.REACT_APP_VERSION || "unset_version";
 export const githubUrlVersion = `${githubUrl}/releases/tag/${version}`;
 export const title = "Swiss QR Bill";
--- layout/index.js
+++ layout/index.js
@@ -1,9 +1,9 @@
 import React from "../_snowpack/pkg/react.js";
 import {version, githubUrlVersion} from "../config.js";
 import Header from "./header.js";
-const Layout = () => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(Header, null), /* @__PURE__ */ React.createElement("div", {
+const Layout = ({children}) => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(Header, null), /* @__PURE__ */ React.createElement("div", {
   className: "container"
-}, props.children), /* @__PURE__ */ React.createElement("footer", null, /* @__PURE__ */ React.createElement("small", null, /* @__PURE__ */ React.createElement("a", {
+}, children), /* @__PURE__ */ React.createElement("footer", null, /* @__PURE__ */ React.createElement("small", null, /* @__PURE__ */ React.createElement("a", {
   href: githubUrlVersion
 }, version))));
 export default Layout;
