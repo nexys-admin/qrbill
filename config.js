2bb2b1e1034a745a87bfe7bfe453a1a4dbe1c76f
johnb8005 <johnb8005@users.noreply.github.com>
2022-02-26 15:28:12 +0000

    deploy: 2871716aa79bcd1f40a1a7b1509f927e16979039

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
+
 export const githubUrl = "https://github.com/nexys-admin/qrbill";
-export const version = process.env.REACT_APP_VERSION || "unset_version";
+export const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unset_version";
+export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
 export const githubUrlVersion = `${githubUrl}/releases/tag/${version}`;
 export const title = "Swiss QR Bill";
