bedd5d8f24e7266300d8c569fc8d3d03cf3513fb
John B <johan@nexys.ch>
2022-05-19 12:50:49 +0200

    fix home link

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -23,7 +23,7 @@ const Header = () => (
       className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white"
     >
       <h5 className="my-0 mr-md-auto font-weight-normal">
-        <Link to={publicUrl + "/"}>{title}</Link>
+        <Link to={"/"}>{title}</Link>
       </h5>
       <nav className="my-2 my-md-0 mr-md-3">
         {menus.map((menu, i) => (
